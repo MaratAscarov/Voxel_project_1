082fd6e1aae38c7391c171550e473f8b62d8314e
MaratAscarov <marat011383@yandex.ru>
2021-11-10 02:06:55 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame as pg
+
+class App:
+    def __init__(self):
+        pass
+        
+    def update(self):
+        pass
+        
+    def draw(self):
+        pass
+        
+    def run(self):
+        pass
+        
+if __name__ == '__main__':
+    app = App()
+    app.run()
