b09f22cf41cfec4c20422c510cf55be9b5490fd1
MaratAscarov <marat011383@yandex.ru>
2021-11-11 10:19:49 +0300

    Edit 1: voxel_render.py

---
+++ voxel_render.py
@@ -0,0 +1,12 @@
+import pygame as pg
+import numpy as np
+from numba as njit
+import math
+
+class VoxelRender:
+    def __init__(self, app):
+        pass
+    
+    def update(self):
+        pass
+        
